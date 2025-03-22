dc4600628db313c39cdb2395763b6fd2cfc4a4aa
Matheus Cardoso <cardosomcds!@gmail.com>
2025-03-22 11:43:27 -0300

    Reorganização dos arquivos.

--- estudos_plsql_alura/packages/PKG.EXCEPTION.sql
+++
@@ -1,8 +0,0 @@
-CREATE OR REPLACE NONEDITIONABLE PACKAGE pkg_exception
-IS
-    e_null EXCEPTION;
-    PRAGMA exception_init(e_null,-1400);
-    
-    e_fk EXCEPTION;
-    PRAGMA exception_init(e_fk,-2291);
-END;
