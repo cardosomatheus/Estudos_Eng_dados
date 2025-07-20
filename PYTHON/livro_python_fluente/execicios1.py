be4b0da5f85091eab9fb0b763e3d2e2d095de0dc
Matheus Cardoso <cardosomcds!@gmail.com>
2025-07-20 11:56:26 -0300

    exercicios do livro python fluente.

---
+++ PYTHON/livro_python_fluente/execicios1.py
@@ -0,0 +1,22 @@
+﻿## Exercício 1
+
+#1. Quantos segundos há em 42 minutos e 42 segundos?
+print(f'{(42 + (42/60)) * 60} segundos.')
+
+#2. Quantas milhas há em 10 quilômetros? Dica: uma milha equivale a 1,61 quilômetro.
+print(f'{10/1.61:.2f} milhas')
+
+#3. Se você correr 10 quilômetros em 42 minutos e 42 segundos, qual é o seu passo médio (tempo por milha em minutos e segundos)? Qual é a sua
+#velocidade média em milhas por hora?
+milhas = 10/1.61 # km para milha
+total_minutos = 42 + (42/60)
+
+velocidade_media_por_minutos = total_minutos/milhas
+minutos  =  int(velocidade_media_por_minutos)
+segundos = int((velocidade_media_por_minutos - minutos) *60)
+
+total_horas = total_minutos/60
+velocidade_media_por_hora = milhas/total_horas
+
+print(f'passo medio de {minutos} minutos e {segundos} segundos.')
+print(f'velocidade média em milhas por hora: {velocidade_media_por_hora:.2f}')
---
+++ PYTHON/livro_python_fluente/exercicio2.py
@@ -0,0 +1,49 @@
+﻿#1. O volume de uma esfera com raio r é . Qual é o volume de uma esfera com raio 5?
+#Fórmula do volume da esfera:
+# V = (4/3) * π * r³
+
+raio = (4/3) * 5**3
+valor_pi = 3.14159
+
+print(f'{raio * valor_pi:.2f} unidade cubicas.')
+
+
+"""2. Suponha que o preço de capa de um livro seja R$ 24,95, mas as livrarias recebem um desconto de 40%. O transporte custa R$ 3,00
+para o primeiro exemplar e 75 centavos para cada exemplar adicional. Qual é o custo totalde atacado para 60 cópias?
+"""
+def calcular_valor_total_capas(valor_por_capa: float, qtd_unidades: int, valor_do_desconto: float):
+
+    valor_total_para_compra_das_capas = valor_por_capa * qtd_unidades 
+    valor_total_das_capas_apos_desconto =  valor_total_para_compra_das_capas - (valor_total_para_compra_das_capas*valor_do_desconto/100)
+
+    valor_total_para_transporte_das_capas = 3 + ((qtd_unidades-1) * 0.75) # (59 capas * 75 centavos) + 1 capa de 3 reais
+    valor_total_da_compra = valor_total_das_capas_apos_desconto+valor_total_para_transporte_das_capas    
+    
+
+    print()
+    print(f'O custo total para transporte de capas é {valor_total_para_transporte_das_capas}') 
+    print(f'Valor após o desconto de 40% {valor_total_da_compra}')
+
+
+calcular_valor_total_capas(valor_por_capa=24.95, qtd_unidades=60, valor_do_desconto=40)
+
+"""3. Se eu sair da minha casa às 6:52 e correr 1 quilômetro a um certo passo (8min15s por quilômetro), então 3 quilômetros 
+a um passo mais rápido (7min12s por quilômetro) e 1 quilômetro no mesmo passo usado em primeiro lugar, que horas chego em casa para o café da manhã
+"""
+from datetime import timedelta, datetime
+
+hora_saida = datetime.strptime('06:52','%H:%M')
+print(hora_saida)
+
+# Ritmos (minutos e segundos separados)
+ritmo1 = timedelta(minutes=8, seconds=15)
+ritmo2 = timedelta(minutes=7, seconds=12)
+ritmo3 = timedelta(minutes=8, seconds=15)
+
+# Distâncias
+tempo_total = ritmo1 + (3 * ritmo2) + ritmo3
+
+# Hora de chegada
+hora_chegada = hora_saida + tempo_total
+
+print(hora_chegada)
