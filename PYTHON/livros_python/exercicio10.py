189e26a10376181eee1311b74656d85aea6ca11d
Matheus Cardoso <cardosomcds!@gmail.com>
2025-07-27 16:22:31 -0300

    loop in list.

---
+++ PYTHON/livros_python/exercicio10.py
@@ -0,0 +1,114 @@
+﻿"""
+4.1 Pizzas: Pense em, pelo menos, três tipos que você gosta. Armazene esses nomes
+de pizza em uma lista e use um loop for para exibir o nome de cada uma.
+    • Modifique seu loop for a fim de que exiba uma frase usando o nome da pizza, em
+    vez de exibir apenas o nome da pizza. Para cada pizza, você deve gerar uma linha de
+    saída com uma simples afirmação como: Gosto de pizza de pepperoni.
+    • Adicione uma linha no final do seu programa, fora do loop for, que ressalte o quanto
+    você gosta de pizza. A saída deve ter três ou mais linhas sobre os tipos de pizza que
+    você gosta e, em seguida, uma frase adicional, como Eu amo pizza!  
+
+"""
+
+pizzas = ['mussarela', 'quatro queijos', 'mussarela']
+
+
+for pizza in pizzas:
+    print(pizza)
+    
+
+""" Modifique seu loop for a fim de que exiba uma frase usando o nome da pizza, em
+    vez de exibir apenas o nome da pizza. Para cada pizza, você deve gerar uma linha de
+    saída com uma simples afirmação como: Gosto de pizza de pepperoni.
+"""
+
+for pizza in pizzas: 
+    print(f'Gosto da pizza de {pizza} !!!')
+print('Como Eu amo pizza!')
+
+
+
+""" 
+4.2 Animais: Pense em, pelo menos, três animais diferentes que compartilhem uma
+    característica comum. Armazene o nome desses animais em uma lista e, em seguida,
+    use um loop for para exibir o nome de cada animal.
+    • Modifique seu programa a fim de exibir uma afirmação sobre cada animal, como Um
+    cachorro seria um ótimo animal de estimação (pet).
+    
+    • Adicione uma linha no final do seu programa, indicando o que esses animais
+    compartilham em comum. Você pode exibir uma frase, como Qualquer um desses
+    animais daria um ótimo animal de estimação!
+"""
+
+animais = ['jabuti', 'tartaruga', 'Iguana']
+for animal in animais:
+    print(f'Como seria bom ter um {animal} como animal de estimação (pet).')
+
+print('Todos são répteis')
+
+
+
+# 4.3 Contando até vinte: Use um loop for para exibir os números de 1 a 20, todos juntos.
+for i in range(1,21):
+    print(f'{i}',end='')
+    
+
+
+# 4.4 Um milhão: Crie uma lista com números de um a um milhão e, em seguida, utilize um loop for para exibi-los.
+lista_1milhao = list(range(1,1000001))
+#for i in lista_1milhao:
+#    print(i)
+
+
+"""
+4.5 Somando um milhão: crie uma lista com números de um a um milhão e, em
+seguida, use min() e max() a fim de garantir que sua lista realmente comece em um e
+termine em um milhão. Além disso, use a função sum() para ver a rapidez com que o
+Python pode efetuar a soma de um milhão de números.
+"""
+print(sum(lista_1milhao))
+print(min(lista_1milhao))
+print(max(lista_1milhao))
+
+
+# 4.6 Números ímpares: Use o terceiro argumento da função range() para criar uma lista com números ímpares de 1 a 20. Use o loop for para exibir cada número.
+
+lista_primos = list(range(1,20))
+
+
+
+from exercicio6 import factorial
+for p in lista_primos:
+    if p <= 1:
+        continue
+
+    valor_fatorial =  factorial(p-1) +1    
+    if valor_fatorial%p == 0:
+        print(p)
+    
+
+# 4.7 Três: Crie uma lista dos múltiplos de 3, de 3 a 30. Use um loop for para exibir os números em sua lista.
+
+range_30 = list(range(1,30,3))
+for value in range_30:
+    print(value)
+
+
+
+
+"""
+4.8 Cubos: Um número elevado à terceira potência é chamado de cubo. Por exemplo,
+no Python, o cubo de 2 é escrito como 2**3. Escreva uma lista dos primeiros 10 cubos
+(ou seja, o cubo de cada número inteiro de 1 a 10) e use um loop for para exibir o
+valor de cada cubo.
+
+
+4.9 Cube Comprehension:
+"""
+value_of_cube = list(range(1,11))
+for value in value_of_cube:
+    cube = value ** 3
+    print(cube)
+
+list_cube_compreenshion = [value**3 for value in value_of_cube]
+print(list_cube_compreenshion)
---
+++ PYTHON/livros_python/exercicio11.py
@@ -0,0 +1,55 @@
+﻿
+
+"""
+4.10 Fatias: Use um dos programas que escreveu neste capítulo, adicione diversas
+linhas ao final do programa para executarem o seguinte:
+    • Exiba a mensagem: Os três primeiros elementos da lista são:. Em seguida, use uma
+    fatia para exibir os três primeiros elementos da lista desse programa.
+    • Exiba a mensagem: O três elementos que ficam no meio da lista são:. Depois, use
+    uma fatia para exibir os três elementos do meio da lista.
+    • Exiba a mensagem: Os três últimos elementos da lista são: Em seguida, utilize uma
+    fatia para exibir os três últimos elementos da lista.
+
+"""
+
+lista = ["Everest","Aconcágua", "Kilimanjaro", "Amazonas",  "Nilo", "Yangtzé", "Brasil","Japão","Canadá",         
+    "Paris", "São Paulo",   "Tóquio","Português", "Inglês","Mandarim"        
+]
+print('Os três primeiros elementos da lista são: \n')
+for value in lista[:3]:
+    print(value)
+
+meio_da_lista = int(len(lista)/2)
+print('O três elementos que ficam no meio da lista são: \n')
+for value in lista[meio_da_lista:meio_da_lista+3]:
+    print(value)
+
+print('Os três últimos elementos da lista são: \n')
+for value in lista[-3:]:
+    print(value)
+
+"""
+4.11 Minhas pizzas, suas pizzas: Comece com o programa do Exercício 4.1 (página 90).
+Faça uma cópia da lista de pizzas e a nomeie como friend_pizzas. Em seguida, siga as
+etapas:
+• Adicione uma pizza nova à lista original.
+• Adicione uma pizza diferente à lista friend_pizzas.
+• Prove que tem duas listas separadas. Exiba a mensagem: Minhas pizzas favoritas
+são:. E, em seguida, use um loop for para exibir a primeira lista. Exiba a mensagem:
+Minhas pizzas favoritas são:. E, em seguida, use um loop for para exibir a segunda
+lista. 
+"""
+pizzas = ['mussarela', 'quatro queijos', 'mussarela']
+pizzas_friends = pizzas.copy()
+
+pizzas.append('calabresa')
+pizzas_friends.append('catupiri')
+
+print('Minhas pizzas favoritas são: \n')
+for pizza in pizzas_friends:
+    print(pizzas)
+    
+print('Minhas pizzas favoritas são: \n')
+for pizza in pizzas_friends:
+    print(pizzas)
+
