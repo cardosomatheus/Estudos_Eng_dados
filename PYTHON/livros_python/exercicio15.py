4409350455f186c7decefe5ac48fa2420bdc5b2e
Matheus Cardoso <cardosomcds!@gmail.com>
2025-07-28 11:51:02 -0300

    Workink dicts.

---
+++ PYTHON/livros_python/exercicio15.py
@@ -0,0 +1,35 @@
+﻿"""
+6.1 Pessoa: Use um dicionário para armazenar informações sobre uma pessoa que você
+conhece. Armazene o nome, sobrenome, idade e a cidade onde mora. Nomeie as
+chaves como first_name, last_name, age e city. Exiba cada informação armazenada em
+seu dicionário.
+
+6.2 Números favoritos: Use um dicionário para armazenar os números favoritos das
+pessoas. Pense em cinco nomes e os utilize como chaves em seu dicionário. Pense em
+um número favorito para cada pessoa e armazene cada um como um valor em seu
+dicionário. Exiba o nome de cada pessoa e seu número favorito. Para que tudo fique
+ainda mais divertido, pergunte a alguns amigos e obtenha alguns dados reais para o
+seu programa.
+
+"""
+pessoa = {
+    'first_name': 'Matheus',
+    'last_name': 'Cardoso',
+    'age': 24,
+    'city': 'Belo Horizonte'
+}
+
+# Exibindo as informações
+print(f"Nome: {pessoa.get('first_name',None)}")
+print(f"Sobrenome: {pessoa.get('last_name',None)}")
+print(f"Idade: {pessoa.get('age',None)}")
+print(f"Cidade: {pessoa.get('city',None)}")
+
+numeros = {'jose': 10,
+           'marcus': 5,
+           'matheus': 8,
+           'ana': 1,
+           'lucia': 2}
+
+for key, value in numeros.items():
+    print(f'o valor favorito de {key} é {value}. \n')
---
+++ PYTHON/livros_python/exercicio16.py
@@ -0,0 +1,38 @@
+﻿users = {
+    'aeinstein': {
+        'first': 'albert',
+        'last': 'einstein',
+        'location': 'princeton',
+    },
+    'mcurie': {
+        'first': 'marie',
+        'last': 'curie',
+        'location': 'paris',
+    },
+}
+for username, user_info in users.items():
+    print(f"\nUsername: {username}")
+    
+    full_name = f"{user_info['first']} {user_info['last']}"
+    location = user_info['location']
+    print(f"\tFull name: {full_name.title()}")
+    print(f"\tLocation: {location.title()}")
+
+
+responses = {}
+# Define uma flag para sinalizar que a pesquisa está ativa
+polling_active = True
+while polling_active:
+# Solicita o nome e a resposta do participante
+    name = input("\nWhat is your name? ")
+    response = input("Which mountain would you like to climb someday? ")
+    # Armazena a resposta no dicionário
+    responses[name] = response
+    # Detecta se mais alguém participará da pesquisa
+    repeat = input("Would you like to let another person respond? (yes/ no) ")
+    if repeat == 'no':
+        polling_active = False
+        # A pesquisa está completa. Mostra os resultados.
+        print("\n--- Poll Results ---")
+    for name, response in responses.items():
+        print(f"{name} would like to climb {response}.")    
